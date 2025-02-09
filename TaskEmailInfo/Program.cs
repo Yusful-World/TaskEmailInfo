7f1413c9c874d2abcfdb4950ef26199d33d64c4e
Abdullahi Yusuf <abdullahiyusuf042000@gmail.com>
2025-02-09 09:54:53 +0100

    added usecors and updated configurations

--- TaskEmailInfo/Program.cs
+++ TaskEmailInfo/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddCors(options => 
     options.AddPolicy("AllowAll", policy => 
         policy.AllowAnyOrigin()
-        .WithMethods("Get", "Post")
+        .AllowAnyMethod()
         .AllowAnyHeader()
     )
 );
@@ -28,6 +28,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors("AllowAll");
 
 
 app.MapGet("/email_info", () =>
@@ -36,7 +37,7 @@ app.MapGet("/email_info", () =>
     {
         RegisteredMail = "abdullahiyusuf182@gmail.com", 
         CurrentDate = DateTime.UtcNow.ToString("o"), 
-        GitHubUrl = ""
+        GitHubUrl = "https://github.com/Yusful-World/TaskEmailInfo.git"
     };
     return Results.Ok(emailInfo);
 })
