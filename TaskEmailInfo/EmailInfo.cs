c661711535e8c6bb573c9ecd79991f8dfc67419e
Abdullahi Yusuf <abdullahiyusuf042000@gmail.com>
2025-02-09 08:52:44 +0100

    Add project files.

---
+++ TaskEmailInfo/EmailInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace TaskEmailInfo
+{
+    public class EmailInfo
+    {
+        public string? RegisteredMail { get; set; }
+        public string? CurrentDate { get; set; }
+        public string? GitHubUrl { get; set; }
+    }
+}
---
+++ TaskEmailInfo/Program.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Refit;
+using TaskEmailInfo;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+app.MapGet("/email_info", () =>
+{
+    EmailInfo emailInfo = new()
+    {
+        RegisteredMail = "abdullahiyusuf182@gmail.com", 
+        CurrentDate = DateTime.UtcNow.ToString("o"), 
+        GitHubUrl = ""
+    };
+    return Results.Ok(emailInfo);
+})
+.WithName("GetEmailInfo")
+.WithOpenApi();
+
+app.Run();
+
